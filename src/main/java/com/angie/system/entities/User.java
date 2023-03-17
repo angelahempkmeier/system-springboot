97297f17d8c0b0089b90deb4b615b1e901719638
aggsdp <aggsdpz@gmail.com>
2023-03-16 22:55:51 -0300

    user entity resources

---
+++ src/main/java/com/angie/system/entities/User.java
@@ -0,0 +1,75 @@
+package com.angie.system.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    private Long id;
+    private String name;
+    private String email;
+    private String fone;
+    private String password;
+
+    public User(){}
+
+    public User(Long id, String name, String email, String fone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.fone = fone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/angie/system/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.angie.system.resources;
+
+import com.angie.system.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail", "479999999", "123");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
