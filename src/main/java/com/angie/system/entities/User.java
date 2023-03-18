666b6833c98eaff5e773cce85c990c4dcd560f9a
aggsdp <aggsdpz@gmail.com>
2023-03-18 16:53:43 -0300

    application teste with h2 database

--- src/main/java/com/angie/system/entities/User.java
+++ src/main/java/com/angie/system/entities/User.java
@@ -1,9 +1,15 @@
 package com.angie.system.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
