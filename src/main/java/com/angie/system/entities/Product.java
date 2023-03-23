e3bb54d09d1104da91e9813835a9369daea5d315
aggsdp <aggsdpz@gmail.com>
2023-03-23 18:23:20 -0300

    product order item

--- src/main/java/com/angie/system/entities/Product.java
+++ src/main/java/com/angie/system/entities/Product.java
@@ -1,5 +1,6 @@
 package com.angie.system.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -20,7 +21,8 @@ public class Product implements Serializable {
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
-
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
     public Product(){}
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
@@ -73,6 +75,14 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
