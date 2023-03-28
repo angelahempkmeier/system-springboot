506d933ea1828d79e277915b63a3cab3a6bfddb4
aggsdp <aggsdpz@gmail.com>
2023-03-28 20:51:39 -0300

    update exception

--- src/main/java/com/angie/system/services/UserService.java
+++ src/main/java/com/angie/system/services/UserService.java
@@ -4,6 +4,7 @@ import com.angie.system.entities.User;
 import com.angie.system.repositories.UserRepository;
 import com.angie.system.services.exceptions.DatabaseException;
 import com.angie.system.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,9 +42,15 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+            
+        }
+
     }
 
     private void updateData(User entity, User obj){
