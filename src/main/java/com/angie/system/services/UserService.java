b907b941c2bce3c747767525bb612d12edb09e9a
aggsdp <aggsdpz@gmail.com>
2023-03-28 20:37:24 -0300

    exceptions

--- src/main/java/com/angie/system/services/UserService.java
+++ src/main/java/com/angie/system/services/UserService.java
@@ -2,6 +2,7 @@ package com.angie.system.services;
 
 import com.angie.system.entities.User;
 import com.angie.system.repositories.UserRepository;
+import com.angie.system.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id){
       Optional<User> obj = repository.findById(id);
-      return obj.get();
+      return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
